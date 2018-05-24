59ba5dbb83924de07c38b271b97e01efeabaaaf7
KingsGitHub <king.tong@westerncape.gov.za>
2018-05-24 09:41:06 +0200

    fixed firebase config import into main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import {store} from './store/index'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
-import {firebaseConfig} from 'firebaseConfig'
+import firebaseConfig from './firebaseConfig.js'
 
 Vue.use(Vuetify, {
   theme: {
